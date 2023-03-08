ba5b80610c11948c1753af4e70900acd415ae07a
Solaris97 <dlwjd15gus@naver.com>
2023-03-08 12:10:09 +0900

    🎉 new project

--- App.js
+++ App.js
@@ -1,391 +1,165 @@
-import AudioRecorderPlayer, {
-  AVEncoderAudioQualityIOSType,
-  AVEncodingOption,
-  AudioEncoderAndroidType,
-  AudioSourceAndroidType,
-  OutputFormatAndroidType,
-} from 'react-native-audio-recorder-player';
-
-import {
-  Dimensions,
-  PermissionsAndroid,
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
 import React, { Component } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { Buffer } from 'buffer';
+import Permissions from 'react-native-permissions';
+import Sound from 'react-native-sound';
+import AudioRecord from 'react-native-audio-record';
+import wavHeaderBytes from 'wavHeaderBytes.js' // <= this just an bytes array
+import axios from 'axios';
 
-import Button from './components/uis/Button';
+export default class App extends Component {
+  sound = null;
+  state = {
+    audioFile: '',
+    recording: false,
+    loaded: false,
+    paused: true,
+  };
+
+  async componentDidMount() {
+    await this.checkPermission();
+
+    function streamAudioChunk({ base64Chunk }) {
+      socket && socket.emit(EVENTS.AUDIO_STREAM_CHUNK, { base64Chunk });
+    }
+    streamAudioChunk({
+      base64Chunk: Buffer.from(wavHeaderBytes).toString("base64"),
+    });
+    const options = {
+      sampleRate: 16000,
+      channels: 1,
+      bitsPerSample: 16,
+      wavFile: 'test.wav',
+    };
+
+    AudioRecord.init(options);
+
+    AudioRecord.on('data', data => {
+      // base64-encoded audio data chunks
+      streamAudioChunk({ base64Chunk: data });
+    });
+  }
+
+  checkPermission = async () => {
+    const p = await Permissions.check('microphone');
+    console.log('permission check', p);
+    if (p === 'authorized') {
+      return;
+    }
+    return this.requestPermission();
+  };
+
+  requestPermission = async () => {
+    const p = await Permissions.request('microphone');
+    console.log('permission request', p);
+  };
+
+  start = () => {
+    console.log('start record');
+    this.setState({ audioFile: '', recording: true, loaded: false });
+    AudioRecord.start();
+  };
+
+  stop = async () => {
+    if (!this.state.recording) {
+      return;
+    }
+    console.log('stop record');
+    let audioFile = await AudioRecord.stop();
+    this.setState({ audioFile, recording: false });
+
+    const base64Data = Buffer.concat(audioPieces).toString('base64');
+
+    console.log(streamAudioChunk);
+
+
+    // const config = {
+    //   headers: { "Content-Type": "multipart/form-data" },
+    // };
+    // //서버로 파일 전송
+    // await axios
+    //   .post("https://192.168.2.3:8000/voiceTest", formData, config)
+    //   .then((res) => {
+    //     //이상없이 데이터 분석이 완료된 경우 "음성 분석" 버튼을 그림검사로 넘어가는 "다음으로" 버튼으로 수정
+    //     console.log("분석 완료!!");
+    //     console.log(res);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+  };
+
+  load = () => {
+    return new Promise((resolve, reject) => {
+      if (!this.state.audioFile) {
+        return reject('file path is empty');
+      }
+
+      this.sound = new Sound(this.state.audioFile, '', error => {
+        if (error) {
+          console.log('failed to load the file', error);
+          return reject(error);
+        }
+        this.setState({ loaded: true });
+        return resolve();
+      });
+    });
+  };
+
+  play = async () => {
+    if (!this.state.loaded) {
+      try {
+        await this.load();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    this.setState({ paused: false });
+    Sound.setCategory('Playback');
+
+    this.sound.play(success => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+      this.setState({ paused: true });
+      // this.sound.release();
+    });
+  };
+
+  pause = () => {
+    this.sound.pause();
+    this.setState({ paused: true });
+  };
+
+  render() {
+    const { recording, paused, audioFile } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Button onPress={this.start} title="Record" disabled={recording} />
+          <Button onPress={this.stop} title="Stop" disabled={!recording} />
+          {paused ? (
+            <Button onPress={this.play} title="Play" disabled={!audioFile} />
+          ) : (
+            <Button onPress={this.pause} title="Pause" disabled={!audioFile} />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#455A64',
-    flexDirection: 'column',
-    alignItems: 'center',
+    justifyContent: 'center',
   },
-  titleTxt: {
-    marginTop: 100,
-    color: 'white',
-    fontSize: 28,
-  },
-  viewRecorder: {
-    marginTop: 40,
-    width: '100%',
-    alignItems: 'center',
-  },
-  recordBtnWrapper: {
+  row: {
     flexDirection: 'row',
-  },
-  viewPlayer: {
-    marginTop: 60,
-    alignSelf: 'stretch',
-    alignItems: 'center',
-  },
-  viewBarWrapper: {
-    marginTop: 28,
-    marginHorizontal: 28,
-    alignSelf: 'stretch',
-  },
-  viewBar: {
-    backgroundColor: '#ccc',
-    height: 4,
-    alignSelf: 'stretch',
-  },
-  viewBarPlay: {
-    backgroundColor: 'white',
-    height: 4,
-    width: 0,
-  },
-  playStatusTxt: {
-    marginTop: 8,
-    color: '#ccc',
-  },
-  playBtnWrapper: {
-    flexDirection: 'row',
-    marginTop: 40,
-  },
-  btn: {
-    borderColor: 'white',
-    borderWidth: 1,
-  },
-  txt: {
-    color: 'white',
-    fontSize: 14,
-    marginHorizontal: 8,
-    marginVertical: 4,
-  },
-  txtRecordCounter: {
-    marginTop: 32,
-    color: 'white',
-    fontSize: 20,
-    textAlignVertical: 'center',
-    fontWeight: '200',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 3,
-  },
-  txtCounter: {
-    marginTop: 12,
-    color: 'white',
-    fontSize: 20,
-    textAlignVertical: 'center',
-    fontWeight: '200',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 3,
+    justifyContent: 'space-evenly',
   },
 });
-
-const screenWidth = Dimensions.get('screen').width;
-
-class Page extends Component {
-  path = Platform.select({
-    ios: undefined,
-    android: undefined,
-
-    // Discussion: https://github.com/hyochan/react-native-audio-recorder-player/discussions/479
-    // ios: 'https://firebasestorage.googleapis.com/v0/b/cooni-ebee8.appspot.com/o/test-audio.mp3?alt=media&token=d05a2150-2e52-4a2e-9c8c-d906450be20b',
-    // ios: 'https://staging.media.ensembl.fr/original/uploads/26403543-c7d0-4d44-82c2-eb8364c614d0',
-    // ios: 'hello.m4a',
-    // android: `${this.dirs.CacheDir}/hello.mp3`,
-  });
-
-  audioRecorderPlayer;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggingIn: false,
-      recordSecs: 0,
-      recordTime: '00:00:00',
-      currentPositionSec: 0,
-      currentDurationSec: 0,
-      playTime: '00:00:00',
-      duration: '00:00:00',
-    };
-
-    this.audioRecorderPlayer = new AudioRecorderPlayer();
-    this.audioRecorderPlayer.setSubscriptionDuration(0.1); // optional. Default is 0.5
-  }
-
-  render() {
-    let playWidth =
-      (this.state.currentPositionSec / this.state.currentDurationSec) *
-      (screenWidth - 56);
-
-    if (!playWidth) {
-      playWidth = 0;
-    }
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.titleTxt}>Audio Recorder Player</Text>
-        <Text style={styles.txtRecordCounter}>{this.state.recordTime}</Text>
-        <View style={styles.viewRecorder}>
-          <View style={styles.recordBtnWrapper}>
-            <Button
-              style={styles.btn}
-              onPress={this.onStartRecord}
-              textStyle={styles.txt}>
-              Record
-            </Button>
-            <Button
-              style={[
-                styles.btn,
-                {
-                  marginLeft: 12,
-                },
-              ]}
-              onPress={this.onPauseRecord}
-              textStyle={styles.txt}>
-              Pause
-            </Button>
-            <Button
-              style={[
-                styles.btn,
-                {
-                  marginLeft: 12,
-                },
-              ]}
-              onPress={this.onResumeRecord}
-              textStyle={styles.txt}>
-              Resume
-            </Button>
-            <Button
-              style={[styles.btn, { marginLeft: 12 }]}
-              onPress={this.onStopRecord}
-              textStyle={styles.txt}>
-              Stop
-            </Button>
-          </View>
-        </View>
-        <View style={styles.viewPlayer}>
-          <TouchableOpacity
-            style={styles.viewBarWrapper}
-            onPress={this.onStatusPress}>
-            <View style={styles.viewBar}>
-              <View style={[styles.viewBarPlay, { width: playWidth }]} />
-            </View>
-          </TouchableOpacity>
-          <Text style={styles.txtCounter}>
-            {this.state.playTime} / {this.state.duration}
-          </Text>
-          <View style={styles.playBtnWrapper}>
-            <Button
-              style={styles.btn}
-              onPress={this.onStartPlay}
-              textStyle={styles.txt}>
-              Play
-            </Button>
-            <Button
-              style={[
-                styles.btn,
-                {
-                  marginLeft: 12,
-                },
-              ]}
-              onPress={this.onPausePlay}
-              textStyle={styles.txt}>
-              Pause
-            </Button>
-            <Button
-              style={[
-                styles.btn,
-                {
-                  marginLeft: 12,
-                },
-              ]}
-              onPress={this.onResumePlay}
-              textStyle={styles.txt}>
-              Resume
-            </Button>
-            <Button
-              style={[
-                styles.btn,
-                {
-                  marginLeft: 12,
-                },
-              ]}
-              onPress={this.onStopPlay}
-              textStyle={styles.txt}>
-              Stop
-            </Button>
-          </View>
-        </View>
-      </SafeAreaView>
-    );
-  }
-
-  // eslint-disable-next-line prettier/prettier
-  onStatusPress = (e) => {
-    const touchX = e.nativeEvent.locationX;
-    console.log(`touchX: ${touchX}`);
-
-    const playWidth =
-      (this.state.currentPositionSec / this.state.currentDurationSec) *
-      (screenWidth - 56);
-    console.log(`currentPlayWidth: ${playWidth}`);
-
-    const currentPosition = Math.round(this.state.currentPositionSec);
-
-    if (playWidth && playWidth < touchX) {
-      const addSecs = Math.round(currentPosition + 1000);
-      this.audioRecorderPlayer.seekToPlayer(addSecs);
-      console.log(`addSecs: ${addSecs}`);
-    } else {
-      const subSecs = Math.round(currentPosition - 1000);
-      this.audioRecorderPlayer.seekToPlayer(subSecs);
-      console.log(`subSecs: ${subSecs}`);
-    }
-  };
-
-  onStartRecord = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const grants = await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-        ]);
-
-        console.log('write external stroage', grants);
-
-        if (
-          grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
-            PermissionsAndroid.RESULTS.GRANTED &&
-          grants['android.permission.READ_EXTERNAL_STORAGE'] ===
-            PermissionsAndroid.RESULTS.GRANTED &&
-          grants['android.permission.RECORD_AUDIO'] ===
-            PermissionsAndroid.RESULTS.GRANTED
-        ) {
-          console.log('permissions granted');
-        } else {
-          console.log('All required permissions not granted');
-
-          return;
-        }
-      } catch (err) {
-        console.warn(err);
-
-        return;
-      }
-    }
-
-    const audioSet = {
-      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
-      AudioSourceAndroid: AudioSourceAndroidType.MIC,
-      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
-      AVNumberOfChannelsKeyIOS: 2,
-      AVFormatIDKeyIOS: AVEncodingOption.aac,
-      OutputFormatAndroid: OutputFormatAndroidType.AAC_ADTS,
-    };
-
-    console.log('audioSet', audioSet);
-
-    const uri = await this.audioRecorderPlayer.startRecorder(
-      this.path,
-      audioSet,
-    );
-
-    // eslint-disable-next-line prettier/prettier
-    this.audioRecorderPlayer.addRecordBackListener((e) => {
-      // console.log('record-back', e);
-      this.setState({
-        recordSecs: e.currentPosition,
-        recordTime: this.audioRecorderPlayer.mmssss(
-          Math.floor(e.currentPosition),
-        ),
-      });
-    });
-    console.log(`uri: ${uri}`);
-  };
-
-  onPauseRecord = async () => {
-    try {
-      const r = await this.audioRecorderPlayer.pauseRecorder();
-      console.log(r);
-    } catch (err) {
-      console.log('pauseRecord', err);
-    }
-  };
-
-  onResumeRecord = async () => {
-    await this.audioRecorderPlayer.resumeRecorder();
-  };
-
-  onStopRecord = async () => {
-    const result = await this.audioRecorderPlayer.stopRecorder();
-    this.audioRecorderPlayer.removeRecordBackListener();
-    this.setState({
-      recordSecs: 0,
-    });
-    console.log(result);
-  };
-
-  onStartPlay = async () => {
-    console.log('onStartPlay', this.path);
-
-    try {
-      const msg = await this.audioRecorderPlayer.startPlayer(this.path);
-
-      //? Default path
-      // const msg = await this.audioRecorderPlayer.startPlayer();
-      const volume = await this.audioRecorderPlayer.setVolume(1.0);
-      console.log(`path: ${msg}`, `volume: ${volume}`);
-
-      // eslint-disable-next-line prettier/prettier
-      this.audioRecorderPlayer.addPlayBackListener((e) => {
-        console.log('playBackListener', e);
-        this.setState({
-          currentPositionSec: e.currentPosition,
-          currentDurationSec: e.duration,
-          playTime: this.audioRecorderPlayer.mmssss(
-            Math.floor(e.currentPosition),
-          ),
-          duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
-        });
-      });
-    } catch (err) {
-      console.log('startPlayer error', err);
-    }
-  };
-
-  onPausePlay = async () => {
-    await this.audioRecorderPlayer.pausePlayer();
-  };
-
-  onResumePlay = async () => {
-    await this.audioRecorderPlayer.resumePlayer();
-  };
-
-  onStopPlay = async () => {
-    console.log('onStopPlay');
-    this.audioRecorderPlayer.stopPlayer();
-    this.audioRecorderPlayer.removePlayBackListener();
-  };
-}
-
-export default Page;
